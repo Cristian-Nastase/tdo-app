36e01ec4b1cdab2501d25597bced8ed548eb2872
Cristian-Nastase <cristianemail@duck.com>
2026-04-05 01:38:09 +0300

    I HATE THIS API

--- src/scripts/list/DragDropTouch.js
+++ src/scripts/list/DragDropTouch.js
@@ -437,16 +437,18 @@ var DragDropTouch;
         };
         return DragDropTouch;
     }());
-    /*private*/ DragDropTouch._instance = new DragDropTouch(); // singleton
     // constants
     DragDropTouch._THRESHOLD = 5; // pixels to move before drag starts
     DragDropTouch._OPACITY = 0.5; // drag image opacity
     DragDropTouch._DBLCLICK = 500; // max ms between clicks in a double click
-    DragDropTouch._CTXMENU = 900; // ms to hold before raising 'contextmenu' event
-    DragDropTouch._ISPRESSHOLDMODE = false; // decides of press & hold mode presence
+    DragDropTouch._CTXMENU = 1200; // ms to hold before raising 'contextmenu' event
+    DragDropTouch._ISPRESSHOLDMODE = true; // decides of press & hold mode presence
     DragDropTouch._PRESSHOLDAWAIT = 400; // ms to wait before press & hold is detected
     DragDropTouch._PRESSHOLDMARGIN = 25; // pixels that finger might shiver while pressing
     DragDropTouch._PRESSHOLDTHRESHOLD = 0; // pixels to move before drag starts
+
+    /*private*/ DragDropTouch._instance = new DragDropTouch(); // singleton
+    
     // copy styles/attributes from drag source to drag image element
     DragDropTouch._rmvAtts = 'id,class,style,draggable'.split(',');
     // synthesize and dispatch an event
